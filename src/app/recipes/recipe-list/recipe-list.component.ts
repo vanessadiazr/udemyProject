5fb1073177922ad7b577bc7a529d67bc0274b76e
Vanessa Ribeiro <vanessa.ribeiro@lisbonnearshore.com>
2019-02-07 13:47:27 +0000

    sec commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.scss']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('o nome do prato', 'a descrição',
+    'https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg'),
+
+    new Recipe('o nome do prato', 'a descrição',
+    'https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg'),
+
+    new Recipe('o nome do prato', 'a descrição',
+    'https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
