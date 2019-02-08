1be8e6fac0fdd3523b4e09f4a9a5c8e480d8dd5b
Vanessa Ribeiro <vanessa.ribeiro@lisbonnearshore.com>
2019-02-08 16:54:28 +0000

    4th commit - local references

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,14 +8,14 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('o nome do prato', 'a descrição',
+    new Recipe('Frigles 1', 'a descrição',
     'https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg'),
 
-    new Recipe('o nome do prato', 'a descrição',
-    'https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg'),
+    new Recipe('Frigles 2', 'a descrição',
+    'https://www.maxpixel.net/static/photo/1x/Food-Kitchen-Meals-Home-Made-Dishes-Recipe-Bio-1175493.jpg'),
 
-    new Recipe('o nome do prato', 'a descrição',
-    'https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg')
+    new Recipe('Frigles 3', 'a descrição',
+    'https://get.pxhere.com/photo/food-cuisine-thai-chili-spicy-vegetable-herb-meat-gourmet-ingredient-sauce-recipe-pepper-delicious-salad-curry-thailand-hot-culture-dinner-dish-restaurant-meal-rice-healthy-appetizer-cooking-lunch-eat-asia-thai-food-vegetarian-food-asian-food-phat-si-io-chinese-food-southeast-asian-food-leaf-vegetable-chow-mein-char-kway-teow-pad-thai-1440945.jpg')
   ];
 
   constructor() { }
